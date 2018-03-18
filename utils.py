d20274c6b69e799caf001e8985e0c32a35d76649
Bogdan Kandra <bogdan.kandra@gmail.com>
2018-03-18 16:58:30 +0200

    Created utils file

---
+++ utils.py
@@ -0,0 +1,32 @@
+"""
+Created on Sun Mar 18 16:51:15 2018
+
+@author: Bogdan
+"""
+
+MARKS = ['.', ',', '?', '!', ':', ';', '(', ')',
+         '[', ']', '...', '\'', '\"', "\''"]
+
+FUNCTION_WORDS = ['about', 'across', 'against', 'along', 'around', 'at',
+                 'behind', 'beside', 'besides', 'by', 'despite', 'down',
+                 'during', 'for', 'from', 'in', 'inside', 'into', 'near', 'of',
+                 'off', 'on', 'onto', 'over', 'through', 'to', 'toward',
+                 'with', 'within', 'without', 'anything', 'everything',
+                 'anyone', 'everyone', 'ones', 'such', 'it', 'itself',
+                 'something', 'nothing', 'someone', 'the', 'some', 'this',
+                 'that', 'every', 'all', 'both', 'one', 'first', 'other',
+                 'next', 'many', 'much', 'more', 'most', 'several', 'no', 'a',
+                 'an', 'any', 'each', 'no', 'half', 'twice', 'two', 'second',
+                 'another', 'last', 'few', 'little', 'less', 'least', 'own',
+                 'and', 'but', 'after', 'when', 'as', 'because', 'if', 'what',
+                 'where', 'which', 'how', 'than', 'or', 'so', 'before', 'since',
+                 'while', 'although', 'though', 'who', 'whose', 'can', 'may',
+                 'will', 'shall', 'could', 'be', 'do', 'have', 'might', 'would',
+                 'should', 'must', 'here', 'there', 'now', 'then', 'always',
+                 'never', 'sometimes', 'usually', 'often', 'therefore',
+                 'however', 'besides', 'moreover', 'though', 'otherwise',
+                 'else', 'instead', 'anyway', 'incidentally', 'meanwhile']
+
+def remove_punctuation(tokenList):
+    result = [token for token in tokenList if token not in MARKS]
+    return result
