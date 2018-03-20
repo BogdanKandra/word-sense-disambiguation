4db7120920e3d3ea837dcba93a9edeaca08a5473
Bogdan Kandra <bogdan.kandra@gmail.com>
2018-03-20 02:35:10 +0200

    Added TODO

--- utils.py
+++ utils.py
@@ -31,6 +31,39 @@ STOPWORDS = ['her', 'most', 'much', 'although', 'an', 'at', 'that', 'along',
                   'be', 'less', 'and', 'without', 'him', 'however', 'by', 
                   'moreover', 'as', 'little', 'everything', 'say']
 
+SENSEVAL_SENSE_DICT = {
+    "HARD1": ["difficult.a.01"],    # not easy, requiring great physical or mental
+    "HARD2": ["hard.a.02",          # dispassionate
+              "difficult.a.01"],
+    "HARD3": ["hard.a.03"],         # resisting weight or pressure
+
+    "interest_1": ["interest.n.01"], # readiness to give attention
+    "interest_2": ["interest.n.03"], # quality of causing attention to be given to
+    "interest_3": ["pastime.n.01"],  # activity, etc. that one gives attention to
+    "interest_4": ["sake.n.01"],     # advantage, advancement or favor
+    "interest_5": ["interest.n.05"], # a share in a company or business
+    "interest_6": ["interest.n.04"], # money paid for the use of money
+
+    "cord": ["line.n.18"],          # something (as a cord or rope) that is long and thin and flexible
+    "formation": ["line.n.01","line.n.03"], # a formation of people or things one beside another
+    "text": ["line.n.05"],                 # text consisting of a row of words written across a page or computer screen
+    "phone": ["telephone_line.n.02"],   # a telephone connection
+    "product": ["line.n.22"],       # a particular kind of product or merchandise
+    "division": ["line.n.29"],      # a conceptual separation or distinction
+
+    "SERVE12": ["serve.v.02"],       # do duty or hold offices; serve in a specific function
+    "SERVE10": ["serve.v.06"], # provide (usually but not necessarily food)
+    "SERVE2": ["serve.v.01"],       # serve a purpose, role, or function
+    "SERVE6": ["service.v.01"]      # be used by; as of a utility
+}
+
+# TODO
+SENSE_SENSEVAL_DICT = {
+        
+        
+        
+        }
+
 def remove_punctuation(tokenList):
     result = [token for token in tokenList if token not in MARKS]
     return result
