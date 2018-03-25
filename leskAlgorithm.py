b734476d64fb16bc53d3c58b9c4e7a16a149e5a1
Bogdan Kandra <bogdan.kandra@gmail.com>
2018-03-25 18:36:37 +0300

    Improved algorithm and documentation

--- leskAlgorithm.py
+++ leskAlgorithm.py
@@ -8,32 +8,45 @@ from nltk.corpus import wordnet as wn
 from nltk.tokenize import word_tokenize as w_tok
 import utils
 
-def compute_overlap(gloss, context):
-    gloss.difference(utils.STOPWORDS)
-    context.difference(utils.STOPWORDS)
-    return len(gloss.intersection(context))
+def simplified_lesk(word, sentence):
+    """Determines the WordNet sense of **word** in the given **context**,
+    using the Simplified Lesk Algorithm. \n
 
-def lesk(word, sentence):
-    senses = wn.synsets(word)
+    Arguments:
+        *word* (str) -- the word to be disambiguated
+
+        *sentence* (str) -- the context phrase which contains the *word* to be
+        disambiguated
+
+    Returns:
+        Synset type -- the WordNet sense of the disambiguated word
+    """
     
-    bestSense = senses[0] # The most frequent sense of the word
+    senses = wn.synsets(word) # Take all WordNet senses of the target word
+    
+    bestSense = senses[0] # Initialise as the most frequent sense of the word
     maxOverlap = 0
     
-    sentTokenized = w_tok(sentence)
+    sentTokenized = w_tok(sentence)  # Tokenize the context phrase
     sentNormalized = utils.remove_punctuation(sentTokenized)
-    context = set(sentNormalized)
+    context = set(sentNormalized).difference(utils.STOPWORDS)  # Remove Stopwords
     
     for sense in senses:
+        # For each sense, build its gloss by taking its definition, examples,
+        # hyponyms and examples of hyponyms
+        # TODO -- Maybe hyponyms shouldn't be included
         gloss = set(utils.remove_punctuation(w_tok(sense.definition())))
         for ex in sense.examples():
             gloss.union(w_tok(ex))
-        overlap = compute_overlap(gloss, context)
+        gloss = gloss.difference(utils.STOPWORDS)
+        overlap = len(gloss.intersection(context))
         
         for h in sense.hyponyms():
             gloss = set(utils.remove_punctuation(w_tok(h.definition())))
             for ex in h.examples():
                 gloss.union(w_tok(ex))
-            overlap += compute_overlap(gloss, context)
+            gloss = gloss.difference(utils.STOPWORDS)
+            overlap += len(gloss.intersection(context))
         
         if overlap > maxOverlap:
             maxOverlap = overlap
@@ -41,9 +54,9 @@ def lesk(word, sentence):
     
     return bestSense
 
-#print(lesk('bank', 'The bank can guarantee deposits will eventually cover future tuition costs because it invests in adjustable-rate mortgage securities.').definition())
-#print(lesk('pine', 'pine cone').definition())
-#print(lesk('bass', 'I am cooking basses').definition())
+print(simplified_lesk('bank', 'The bank can guarantee deposits will eventually cover future tuition costs because it invests in adjustable-rate mortgage securities.').definition())
+print(simplified_lesk('pine', 'pine cone').definition())
+print(simplified_lesk('bass', 'I am cooking basses').definition())
 
 #for sin in wn.synsets('bank'):
 #    print(sin.definition())
